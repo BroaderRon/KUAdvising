690dda6ebaa7ed754078ff61b9cf0c52081e9f94
joe <joe@joe.com>
2020-04-16 12:50:02 -0400

    starting backend testing

---
+++ backend/testbackend.py
@@ -0,0 +1,13 @@
+import unittest
+import app
+
+
+class testAddCourse(unittest.TestCase):
+    def test_course_list(self):
+        return add_course()
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
