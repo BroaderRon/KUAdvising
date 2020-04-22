2b742e2da60af923efce74ea9d967775ec5c4aa6
Devon <devonblizard@gmail.com>
2020-04-22 18:15:16 -0400

    Create The IT Component
    Move all IT checksheet items over to the new it component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component'; // Add this
 import { CSComponent } from './checksheet/cs/cs.component'; // Add this
+import { ITComponent } from './checksheet/it/it.component'; // Add this
 import { ChecksheetComponent } from './checksheet/checksheet.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },              // Add this
   { path: 'checksheet', component: ChecksheetComponent } ,
-  { path: 'checksheet/CS', component: CSComponent }           // Add this
+  { path: 'checksheet/CS', component: CSComponent },           // Add this
+  { path: 'checksheet/IT', component: ITComponent }           // Add this
 ];
 
 @NgModule({
--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../http.service';
 import { Router } from '@angular/router';
 import { DataService } from "../cur-user.service";
-import { FormBuilder, FormGroup, FormArray, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -12,11 +11,8 @@ import { FormBuilder, FormGroup, FormArray, FormControl, Validators } from '@ang
 export class HomeComponent implements OnInit {
 
   Stus: Object;
-  form: FormGroup;
 
-  constructor(private _http: HttpService,private router: Router,private data: DataService, private fb: FormBuilder) {
-    this.form = this.fb.group({
-      checkArray: this.fb.array([])})
+  constructor(private _http: HttpService,private router: Router,private data: DataService) {
    }
   
 
@@ -40,39 +36,4 @@ export class HomeComponent implements OnInit {
     this.router.navigate(['./checksheet']);
   }
 
-  Data: Array<any> = [
-    { name: 'CSC 125: DISCRETE MATH FOR CS I', CourseNum: '125', Dept: 'CSC' },
-    { name: 'CSC 130: IT FUNDAMENTALS', CourseNum: '130', Dept: 'CSC' },
-    { name: 'CSC 135: COMP SCI I', CourseNum: '135', Dept: 'CSC' },
-    { name: 'CSC 136: COMP SCI II', CourseNum: '136', Dept: 'CSC' },
-    { name: 'CSC 220: OO MULTIMEDIA PROG.', CourseNum: '220', Dept: 'CSC' },
-    { name: 'CSC 223: ADVANCED SCIENTIFIC PROG. ', CourseNum: '223', Dept: 'CSC' },
-    { name: 'CSC 242: WEB PROGRAMMING', CourseNum: '242', Dept: 'CSC' },
-    { name: 'CSC 252: UNIX SCRIPTING AND ADMINISTRATION', CourseNum: '252', Dept: 'CSC' },
-    { name: 'CSC 253: IT SYSTEMS', CourseNum: '253', Dept: 'CSC' },
-    { name: 'CSC 273: COMPUTING SYSTEMS INTEGRATION', CourseNum: '273', Dept: 'CSC' },
-    { name: 'CSC 311: COMPUTER NETWORKS', CourseNum: '311', Dept: 'CSC' },
-    { name: 'CSC 341: INFORMATION SECURITY', CourseNum: '341', Dept: 'CSC' },
-    { name: 'CSC 356: INTRODUCTION TO DATABASE SYS', CourseNum: '356', Dept: 'CSC' },
-    { name: 'CSC 354: SOFTWARE ENGINEERING I', CourseNum: '354', Dept: 'CSC' },
-    { name: 'CSC 355: SOFTWARE ENGINEERING II', CourseNum: '355', Dept: 'CSC' }
-    
-  ];
-
-  onCheckboxChange(e) {
-    const checkArray: FormArray = this.form.get('checkArray') as FormArray;
-  
-    if (e.target.checked) {
-      checkArray.push(new FormControl(e.target.value));
-    } else {
-      let i: number = 0;
-      checkArray.controls.forEach((item: FormControl) => {
-        if (item.value == e.target.value) {
-          checkArray.removeAt(i);
-          return;
-        }
-        i++;
-      });
-    }
-  }
 }
