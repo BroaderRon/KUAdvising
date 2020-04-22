af27dc2c37eb4760119eb027f4901c17209c281c
Devon <devonblizard@gmail.com>
2020-04-22 17:51:46 -0400

    update array

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -41,21 +41,21 @@ export class HomeComponent implements OnInit {
   }
 
   Data: Array<any> = [
-    { name: 'CSC 125: DISCRETE MATH FOR CS I', value: 'CSC 125' },
-    { name: 'CSC 130: IT FUNDAMENTALS', value: 'CSC 130' },
-    { name: 'CSC 135: COMP SCI I', value: 'CSC 135' },
-    { name: 'CSC 136: COMP SCI II', value: 'CSC 136' },
-    { name: 'CSC 220: OO MULTIMEDIA PROG.', value: 'CSC 220' },
-    { name: 'CSC 223: ADVANCED SCIENTIFIC PROG. ', value: 'CSC 223' },
-    { name: 'CSC 242: WEB PROGRAMMING', value: 'CSC 242' },
-    { name: 'CSC 252: UNIX SCRIPTING AND ADMINISTRATION', value: 'CSC 252' },
-    { name: 'CSC 253: IT SYSTEMS', value: 'CSC 253' },
-    { name: 'CSC 273: COMPUTING SYSTEMS INTEGRATION', value: 'CSC 273' },
-    { name: 'CSC 311: COMPUTER NETWORKS', value: 'CSC 311' },
-    { name: 'CSC 341: INFORMATION SECURITY', value: 'CSC 341' },
-    { name: 'CSC 356: INTRODUCTION TO DATABASE SYS', value: 'CSC 356' },
-    { name: 'CSC 354: SOFTWARE ENGINEERING I', value: 'CSC 354' },
-    { name: 'CSC 355: SOFTWARE ENGINEERING II', value: 'CSC 355' }
+    { name: 'CSC 125: DISCRETE MATH FOR CS I', CourseNum: '125', Dept: 'CSC' },
+    { name: 'CSC 130: IT FUNDAMENTALS', CourseNum: '130', Dept: 'CSC' },
+    { name: 'CSC 135: COMP SCI I', CourseNum: '135', Dept: 'CSC' },
+    { name: 'CSC 136: COMP SCI II', CourseNum: '136', Dept: 'CSC' },
+    { name: 'CSC 220: OO MULTIMEDIA PROG.', CourseNum: '220', Dept: 'CSC' },
+    { name: 'CSC 223: ADVANCED SCIENTIFIC PROG. ', CourseNum: '223', Dept: 'CSC' },
+    { name: 'CSC 242: WEB PROGRAMMING', CourseNum: '242', Dept: 'CSC' },
+    { name: 'CSC 252: UNIX SCRIPTING AND ADMINISTRATION', CourseNum: '252', Dept: 'CSC' },
+    { name: 'CSC 253: IT SYSTEMS', CourseNum: '253', Dept: 'CSC' },
+    { name: 'CSC 273: COMPUTING SYSTEMS INTEGRATION', CourseNum: '273', Dept: 'CSC' },
+    { name: 'CSC 311: COMPUTER NETWORKS', CourseNum: '311', Dept: 'CSC' },
+    { name: 'CSC 341: INFORMATION SECURITY', CourseNum: '341', Dept: 'CSC' },
+    { name: 'CSC 356: INTRODUCTION TO DATABASE SYS', CourseNum: '356', Dept: 'CSC' },
+    { name: 'CSC 354: SOFTWARE ENGINEERING I', CourseNum: '354', Dept: 'CSC' },
+    { name: 'CSC 355: SOFTWARE ENGINEERING II', CourseNum: '355', Dept: 'CSC' }
     
   ];
 
