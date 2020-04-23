9ba6e8d0ebc0bd8506bb049521de9b724aa52dc1
Ronald <rmark385@live.kutztown.edu>
2020-04-23 00:10:28 -0400

    postcourse in cs

--- frontend/src/app/checksheet/cs/cs.component.ts
+++ frontend/src/app/checksheet/cs/cs.component.ts
@@ -3,7 +3,7 @@ import {CourseData} from '../courseSubmit'
 import { FormGroup, FormBuilder, FormArray, FormControl } from '@angular/forms';
 import { DataService } from 'src/app/cur-user.service';
 import { HttpService } from 'src/app/http.service';
-import { EnrollData } from '../enrollSubmit';
+import { EnrollData, EnrollResData } from '../enrollSubmit';
 
 @Component({
   selector: 'app-cs',
@@ -43,11 +43,12 @@ export class CSComponent implements OnInit {
    this.enableStat = tmp;
   }
 
-  onCheckboxChange(e,name,coursenum,dept,grade,semester,box) {
+  onCheckboxChange(e,name,coursenum,dept,grade,semester,box,cat) {
     const checkArray: FormArray = this.form.get('checkArray') as FormArray;
     console.log(name)
     if (e.target.checked) {
       checkArray.push(new FormControl(e.target.value));
+      this.submitEnroll(name,coursenum,dept,grade,semester,box,cat)
     } else {
       let i: number = 0;
       checkArray.controls.forEach((item: FormControl) => {
@@ -81,5 +82,31 @@ export class CSComponent implements OnInit {
     else{
       this.enableStat[index] = true
     }
+  }
+  submitEnroll(name,coursenum: number,dept,grade,semester,box,cat){
+    console.log(semester)
+      var enrollSub = new EnrollData
+      enrollSub.sid = this.message
+      enrollSub.Dept = dept
+      enrollSub.CourseNum = coursenum
+      enrollSub.Cat = cat
+      enrollSub.Grade = grade
+      enrollSub.Semester = semester
+      this._http.postEnroll(enrollSub).subscribe((res: EnrollResData)=> {
+        let eResData = res;
+        console.log(eResData.RESULT)
+        if(eResData.RESULT == 'FALSE'){
+          var courseSub = new CourseData;
+          courseSub.CourseNum = coursenum
+          courseSub.Dept = dept
+          courseSub.Name = name
+          this._http.postCourse(courseSub).subscribe((res: CourseData) =>{
+            console.log(res);
+            this.submitEnroll(name,coursenum,dept,grade,semester,box,cat)
+          })
+          
+        }
+        
+      });
   }
 }
