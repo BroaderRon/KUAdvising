e999260d092325aa4bcf60bfb9de81057714c0e4
Ronald <rmark385@live.kutztown.edu>
2020-04-28 06:24:25 -0400

    a

---
+++ frontend/server.js
@@ -0,0 +1,14 @@
+const express= requre('express'),
+     path = require('path');
+const app = express();
+
+app.use(express.static('./dist/frontend'));
+
+app.get('/*' , (req,res)=>{
+    res.sendFile(path.join(__dirname,'/dist/frontend/index.html'));
+});
+
+app.listen(process.env.PORT || 8080,()=>{
+    console.log('server started')
+})
+
